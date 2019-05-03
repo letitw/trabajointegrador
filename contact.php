b04aa034f9fbe3484a3265356934fe8522393cb4
martudaniele <danielemartina@hotmail.com>
2019-05-03 15:07:23 -0300

    pagina de contacto hecha, falta responsive

---
+++ contact.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href= "contact.css">
+    <title>Contacto</title>
+  </head>
+  <body>
+
+  <header class="header">
+        <div class="logo"><a href="home.php">Wish</a></div>
+        <nav class="main_nav">
+          <ul>
+            <li><a href="ropa.php">ROPA</a></li>
+            <li><a href="accesorios.php">ACCESORIOS</a></li>
+            <li><a href="contacto.html">CONTACTO</a></li>
+          </ul>
+        </nav>
+        <div class="search-box">
+          <input type="search" name="" value="" class="search_input">
+          <button type="submit" name="button" id="search_button">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+        <div class="login-registro">
+          <ul>
+            <li><i class="fas fa-unlock"></i></li>
+            <li><a href="login.php">Login</a></li>
+            <li><i class="fas fa-user"></i></li>
+            <li><a href="registro.php">Registrarse</a></li>
+          </ul>
+
+        </div>
+        <div class="shopping">
+          <ul>
+            <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+            <li><a href="#"><i class="far fa-heart"></i></a></li>
+
+
+
+          </ul>
+        </div>
+        <label class="hamb" for="toggle">&#9776;</label>
+        <input type="checkbox" id="toggle">
+        <div class="nuevo_menu">
+          <a href="ropa.php">ROPA</a>
+          <a href="accesorios.php">ACCESORIOS</a>
+          <a href="contacto.html">CONTACTO</a>
+          <a href="login.php">LOGIN</a>
+          <a href="registro.php">REGISTRARSE</a>
+
+        </div>
+      </header>
+
+<div class="mapa">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52563.438528236045!2d-58.45603509521369!3d-34.5734283494439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb59c771eb933%3A0x6b3113b596d78c69!2sPalermo%2C+CABA!5e0!3m2!1ses-419!2sar!4v1556904085528!5m2!1ses-419!2sar" width="100%" height="531px" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<div class="contact">
+  <div class="container">
+    <div class="form_title">
+      COMUNICATE CON NOSOTROS
+    </div>
+    <div class="form_content">
+      <form class="" action="" method="post">
+        <div class="inputs">
+          <input type="text" class="form_input" placeholder="Nombre" required>
+            <input type="email" class="form_input" placeholder="E-mail" required>
+              <input type="text" class="form_input" placeholder="Asunto">
+        </div>
+        <textarea  class="form_text"name="form_text" rows="8" cols="80" placeholder="Mensaje"></textarea>
+        <button class="form_button" type="submit" name="button">ENVIAR</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<footer class="pie">
+  <div class="columna-pie">
+    <div class="logo-pie">
+      <a href="home.php">Wish</a>
+    </div>
+    <div class="nav-pie">
+      <ul>
+        <li>
+          <a href="ropa.php">ROPA</a>
+        </li>
+        <li>
+          <a href="accesorios.php">ACCESORIOS</a>
+        </li>
+        <li>
+          <a href="contacto.html">CONTACTO</a>
+        </li>
+        <li>
+          <a href="faqs.html">PREGUNTAS FRECUENTES</a>
+        </li>
+      </ul>
+    </div>
+    <div class="redes-pie">
+      <ul>
+        <li>
+          <a href="#">
+            <i class="fab fa-pinterest"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="copyright">
+      © 2019 Wish Inc. All rights reserved
+    </div>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
